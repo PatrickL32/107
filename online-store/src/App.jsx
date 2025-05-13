b2dc61a6dd235fea6a761e49691d4f301ba41368
PatrickL32 <Jedi.92.lewis@gmail.com>
2025-05-12 20:10:14 -0700

    first commit

---
+++ online-store/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from'./pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello world from react.js</h1>
+
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ online-store/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+//Import section
+import'./Home.css'
+
+
+//Functional component
+function Home() {
+    return(
+    <div className ="home">
+
+        <div className ="home-text">
+        <h1>Organika(Home.jsx)</h1>
+        <h3> Fresh food,healthie life.</h3>
+        </div>
+
+    </div>
+    )
+} 
+
+//Export section
+export default Home
